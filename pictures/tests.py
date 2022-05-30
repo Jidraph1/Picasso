cf571caec8d8002d03b596c77fd625822b15a4c9
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-30 04:14:26 +0300

    Image Testcase

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from .models import Location, categories, Image
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.James= Image(id = '1', image = 'example.jpg', name = 'James', description ='This is one of my favourite photos', location ='Nairobi', categories ='Portrait')
