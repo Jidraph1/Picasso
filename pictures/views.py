75ddd54b4d78371aa82c91561ff91e7c1406a903
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-30 05:52:22 +0300

    display views

--- pictures/views.py
+++ pictures/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Image,Location
 
 # Create your views here.
 def index(request):
 
     return render(request, 'index.html')
 
+def display(request):
+    images = Image.objects.all()
+    location = Location.objects.all()
+    return render(request, 'display.html',{"images":images, "location":location})
