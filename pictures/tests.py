3b87c693778f7a4a8e2d6a69df33c646b31a2fee
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-30 04:16:56 +0300

    test instance Image

--- pictures/tests.py
+++ pictures/tests.py
@@ -6,3 +6,13 @@ class ImageTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.James= Image(id = '1', image = 'example.jpg', name = 'James', description ='This is one of my favourite photos', location ='Nairobi', categories ='Portrait')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.James,Image)) 
+    
+     # Testing Save Method
+    def test_save_method(self):
+        self.James.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)  
