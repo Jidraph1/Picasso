9a9ae4299da9ac52867da53aeec43294fa433f04
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-30 05:46:25 +0300

    django admin

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Location, categories, Image
 
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(categories)
+admin.site.register(Image)
