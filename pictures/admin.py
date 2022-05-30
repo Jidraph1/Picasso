9fdebb7f7c2426ec40b43bb2ab4697de01792007
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-30 13:22:55 +0300

    nav search bar

--- pictures/admin.py
+++ pictures/admin.py
@@ -7,5 +7,5 @@ class ImageAdmin(admin.ModelAdmin):
     filter_horizontal =('categories',)
 
 admin.site.register(Location)
+admin.site.register(Image,ImageAdmin)
 admin.site.register(categories)
-admin.site.register(Image)
