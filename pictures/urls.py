85ed264c2604616e1bcc0c6304735d7489ef6973
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-30 13:09:16 +0300

    media URl

--- pictures/urls.py
+++ pictures/urls.py
@@ -4,6 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('display/',views.display, name='display'),
     
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
